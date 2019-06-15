5bb62e3bdb930ae239aca93b88f266ea1daab24d
Hikarien <lgy13564515118@gmail.com>
2019-06-15 16:08:24 +0800

    Initial commit

---
+++ MqttClient/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Subscribing;
+
+namespace MqttClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+        var optionbuilder = new MqttClientOptionsBuilder()
+            .WithClientId("client_1")
+            .WithTcpServer("127.0.0.1", 3883);
+
+            var optionbuilder2 = new MqttClientOptionsBuilder()
+                .WithClientId("client_2")
+                .WithTcpServer("127.0.0.1", 3883);
+
+            var mqttClient = new MqttFactory().CreateMqttClient();
+            var mqttClient2 = new MqttFactory().CreateMqttClient();
+
+            var ret = await mqttClient.ConnectAsync(optionbuilder.Build());
+            Console.WriteLine($"client_1 result : {ret.ResultCode}");
+
+            var ret2 = await mqttClient2.ConnectAsync(optionbuilder2.Build());
+            Console.WriteLine($"client_2 result : {ret2.ResultCode}");
+
+
+            var subcriberet = await mqttClient2.SubscribeAsync(new MqttClientSubscribeOptions()
+            {
+                TopicFilters = new List<TopicFilter>
+                {
+                    new TopicFilter()
+                    {
+                        Topic = "abc"
+                    }
+                }
+            });
+            foreach (var item in subcriberet.Items)
+            {
+                Console.WriteLine($"Topic:{item.TopicFilter.Topic}, result:{item.ResultCode}");
+            }
+
+            mqttClient2.UseApplicationMessageReceivedHandler((e) =>
+            {
+                var msg = e.ApplicationMessage;
+                var topic = msg.Topic;
+                var payload = msg.ConvertPayloadToString();
+                Console.WriteLine($"clientId:{e.ClientId}, topic:{topic}, payload:{payload}");
+            });
+
+            while (true)
+            {
+                var input = Console.ReadLine().ToLower().Trim();
+                switch (input)
+                {
+                    case "exit":
+                        mqttClient.Dispose();
+                        mqttClient2.Dispose();
+                        return;
+                    default:
+                        await mqttClient.PublishAsync(new MqttApplicationMessage()
+                        {
+                            Topic = "abc",
+                            Payload = Encoding.UTF8.GetBytes(DateTime.Now.ToString())
+
+                        }, CancellationToken.None);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ MqttServer/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Diagnostics;
+using MQTTnet.Server;
+
+namespace MqttServer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var optionbuilder = new MqttServerOptionsBuilder()
+                .WithDefaultEndpointPort(3883)
+                .WithConnectionValidator((valid) =>
+            {
+
+
+            });
+
+            var server_logger = new MqttNetLogger("server logger");
+            server_logger.LogMessagePublished += (sender, e) =>
+            {
+
+                //Console.WriteLine(e.TraceMessage.ToString());
+            };
+
+            var mqttServer = new MqttFactory().CreateMqttServer(server_logger);
+
+            mqttServer.StartedHandler = new MqttServerStartedHandlerDelegate((e) =>
+            {
+                Console.WriteLine("mqtt server started!");
+            });
+
+            mqttServer.StoppedHandler = new MqttServerStoppedHandlerDelegate((e) =>
+            {
+                Console.WriteLine("mqtt server stopped!");
+            });
+
+            mqttServer.UseClientConnectedHandler((eventArgs) =>
+            {
+                Console.WriteLine($"client Connected ClientId:{eventArgs.ClientId}");
+            });
+
+            mqttServer.UseClientDisconnectedHandler((eventArgs) =>
+            {
+                Console.WriteLine($"client Disconnected ClientId:{eventArgs.ClientId} Type:{eventArgs.DisconnectType}");
+            });
+
+            mqttServer.UseApplicationMessageReceivedHandler((eventArgs) =>
+            {
+                var msg = eventArgs.ApplicationMessage;
+                var topic = msg.Topic;
+                var payload = msg.ConvertPayloadToString();
+
+                Console.WriteLine($"Topic:{topic}\r\nPayload:{payload}");
+            });
+
+            mqttServer.ClientSubscribedTopicHandler = new MqttServerClientSubscribedHandlerDelegate((e) =>
+            {
+                Console.WriteLine($"topic subscribed ClientId:{e.ClientId} Topic:{e.TopicFilter.ToString()}");
+            });
+
+            mqttServer.ClientUnsubscribedTopicHandler = new MqttServerClientUnsubscribedTopicHandlerDelegate((e) =>
+            {
+                Console.WriteLine($"topic ubsubscribed ClientId:{e.ClientId} Topic:{e.TopicFilter.ToString()}");
+            });
+
+            await mqttServer.StartAsync(optionbuilder.Build());
+
+            Console.WriteLine("输入exit退出");
+            var input = string.Empty;
+            while (true)
+            {
+                input = Console.ReadLine().ToLower().Trim();
+                if (input == "exit") break;
+
+                switch (input)
+                {
+                    case "exit":
+                        await mqttServer.StopAsync();
+                        return;
+                    case "client":
+                        var clients = await mqttServer.GetClientStatusAsync();
+                        if (clients.Count == 0)
+                        {
+                            Console.WriteLine("no client");
+                            continue;
+                        }
+
+                        foreach (var client in clients)
+                        {
+                            Console.WriteLine($"clientid:{client.ClientId}");
+                        }
+                        break;
+                    case "msg":
+                        var msgs = await mqttServer.GetRetainedApplicationMessagesAsync();
+                        if (msgs.Count == 0)
+                        {
+                            Console.WriteLine("no message");
+                        }
+
+                        foreach (var item in msgs)
+                        {
+                            Console.WriteLine($"Topic:{item.Topic}, payload:{item.ConvertPayloadToString()}");
+                        }
+                        break;
+                    case "session":
+                        var mqttSessions = await mqttServer.GetSessionStatusAsync();
+                        if (mqttSessions.Count == 0)
+                        {
+                            Console.WriteLine("no session");
+                            continue;
+                        }
+                        foreach (var item in mqttSessions)
+                        {
+                            Console.WriteLine($"ClientId:{item.ClientId},PendingMsgCnt:{item.PendingApplicationMessagesCount}");
+                        }
+                        break;
+                    default:
+                        await Task.Delay(100);
+                        break;
+                }
+            }
+        }
+    }
+}
